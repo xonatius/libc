91e392bc85e3ffd79d371bdf42bc5cf538488ff0
Deploy from CI <>
2020-07-20 23:43:31 +0000

    Deploy dce0ec0adb8f8ef711ea6b54e90429972f6077a0 to gh-pages

---
+++ riscv32imac-unknown-none-elf/search-index.js
@@ -0,0 +1,4 @@
+var searchIndex = JSON.parse('{\
+"libc":{"doc":"libc - Raw FFI bindings to platforms\' system libraries","i":[],"p":[]}\
+}');
+addSearchOptions(searchIndex);initSearch(searchIndex);
